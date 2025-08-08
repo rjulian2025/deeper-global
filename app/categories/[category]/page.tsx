eb7abeb0074854b67a776054eaec8e93a5f68efc
Rick Julian <rickjulian@Ricks-MacBook.local>
2025-08-07 21:49:44 -0400

    fix: update categories page to use new QuestionCard interface

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -82,7 +82,7 @@ async function CategoryContent({ params }: Props) {
         {/* Questions Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {questions.map((question) => (
-            <QuestionCard key={question.id} question={question} showCategory={false} />
+            <QuestionCard key={question.id} {...question} />
           ))}
         </div>
       </div>
